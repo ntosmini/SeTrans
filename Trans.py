a54b185db3787897f2ddb52a08bf6c2b32a2f1cc
jungmini <mins0428@gmail.com>
2021-08-11 12:42:01 +0900

    NtosMini

--- Trans.py
+++ Trans.py
@@ -76,121 +76,122 @@ def multiSelenium(process):
 			response = requests.post(NtosUrl, data=data)
 
 
-
-		#result = "".join(sampleText)
 		result = response.text
-		
-		if result == "test" :
-			if process == "1" :
-				print("실행명령")
-			else :
-				print("중지")
-		elif result == "error" :
-			print('에러')
-		elif result == "not" :
-			print('번역없음')
+		if result == "" :
+			exit()
 		else :
-			
-			#번역시작
-			resultList = result.split("@@@")
-			number = 0
-			itemcodeArr = []
-			itemnameArr = []
 
-			for val in resultList :
-				(itemcode,itemname) = val.split("|@|")
-				itemcodeArr.insert(number, itemcode)
-				itemnameArr.insert(number, itemname.replace("\n", " "))
-				number = number +1
-
-			TransItemCode = "|@|".join(itemcodeArr)
-			TransItemName = "\n\n\n\n".join(itemnameArr)
-			"""
-			f = open("C:/xampp/htdocs/_Ntos/_TransSelenium/test_"+process+".txt", "w",encoding='UTF-8')
-			f.write(TransItemName)
-			f.close()
-			"""
-			"""
-			print('번역실행'+TransItemName)
-			"""
-			"""
-			options = webdriver.ChromeOptions()
-			options.add_argument('window-size=1920,1080')
-			options.add_argument('HEADLESS')
-			"""
-			chrome_options = webdriver.ChromeOptions()
-			chrome_options.add_argument('HEADLESS')
-			chrome_options.add_argument("window-size=1920,1080")
-
-			if TransSite == "Papago" :
-				URL = 'https://papago.naver.com/?'+ str(TransLeng)
+			if result == "test" :
+				if process == "1" :
+					print("실행명령")
+				else :
+					print("중지")
+			elif result == "error" :
+				print('에러')
+			elif result == "not" :
+				print('번역없음')
 			else :
-				URL = 'https://translate.google.com/?'+ str(TransLeng)
-			if Server == "win" :
-				driver = webdriver.Chrome('C:/xampp/htdocs/_Ntos/_TransSelenium/chromedriver.exe', chrome_options=chrome_options)
-				#driver = webdriver.Chrome('C:/xampp/htdocs/_Ntos/chromedriver.exe', options=options)
-				#driver = webdriver.Chrome('C:/xampp/htdocs/_Ntos/chromedriver.exe')
-			else :	#ubuntu
-				driver = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
-			driver.implicitly_wait(3)
+				
+				#번역시작
+				resultList = result.split("@@@")
+				number = 0
+				itemcodeArr = []
+				itemnameArr = []
 
-			driver.get(url=URL)
-			driver.implicitly_wait(3)
+				for val in resultList :
+					(itemcode,itemname) = val.split("|@|")
+					itemcodeArr.insert(number, itemcode)
+					itemnameArr.insert(number, itemname.replace("\n", " "))
+					number = number +1
 
-			#종료
-			"""
-			def DriverQuit():
-				terminate(process)
-				driver.close()
+				TransItemCode = "|@|".join(itemcodeArr)
+				TransItemName = "\n\n\n\n".join(itemnameArr)
+				"""
+				f = open("C:/xampp/htdocs/_Ntos/_TransSelenium/test_"+process+".txt", "w",encoding='UTF-8')
+				f.write(TransItemName)
+				f.close()
+				"""
+				"""
+				print('번역실행'+TransItemName)
+				"""
+				"""
+				options = webdriver.ChromeOptions()
+				options.add_argument('window-size=1920,1080')
+				options.add_argument('HEADLESS')
+				"""
+				chrome_options = webdriver.ChromeOptions()
+				chrome_options.add_argument('HEADLESS')
+				chrome_options.add_argument("window-size=1920,1080")
 
-			DriverJob = threading.Timer(50, DriverQuit)
-			DriverJob.start()
-			"""
+				if TransSite == "Papago" :
+					URL = 'https://papago.naver.com/?'+ str(TransLeng)
+				else :
+					URL = 'https://translate.google.com/?'+ str(TransLeng)
+				if Server == "win" :
+					driver = webdriver.Chrome('C:/xampp/htdocs/_Ntos/_TransSelenium/chromedriver.exe', chrome_options=chrome_options)
+					#driver = webdriver.Chrome('C:/xampp/htdocs/_Ntos/chromedriver.exe', options=options)
+					#driver = webdriver.Chrome('C:/xampp/htdocs/_Ntos/chromedriver.exe')
+				else :	#ubuntu
+					driver = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
+				driver.implicitly_wait(3)
 
-			if TransSite == "Papago" :
-				input_box = driver.find_element_by_xpath('/html/body/div/div/div[1]/section/div/div[1]/div[1]/div/div[3]/label/textarea')
-			else :
-				input_box = driver.find_element_by_xpath('/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[1]/span/span/div/textarea')
-			time.sleep(1)
-			
-			input_box.send_keys(TransItemName)
-			#pyperclip.copy(TransItemName)
-			#input_box.send_keys(Keys.CONTROL, 'v')
+				driver.get(url=URL)
+				driver.implicitly_wait(3)
 
-			#driver.implicitly_wait(7)
-			time.sleep(10)
+				#종료
+				"""
+				def DriverQuit():
+					terminate(process)
+					driver.close()
 
-			if TransSite == "Papago" :
-				trans_box = driver.find_element_by_xpath('/html/body/div/div/div[1]/section/div/div[1]/div[2]/div/div[5]/div')
-			else :
-				trans_box = driver.find_element_by_xpath('/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div[5]/div/div[1]')
+				DriverJob = threading.Timer(50, DriverQuit)
+				DriverJob.start()
+				"""
 
-			#driver.find_element_by_xpath('/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div[5]/div/div[4]/div[2]/div/span/button/i').click()
+				if TransSite == "Papago" :
+					input_box = driver.find_element_by_xpath('/html/body/div/div/div[1]/section/div/div[1]/div[1]/div/div[3]/label/textarea')
+				else :
+					input_box = driver.find_element_by_xpath('/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[1]/span/span/div/textarea')
+				time.sleep(1)
+				
+				input_box.send_keys(TransItemName)
+				#pyperclip.copy(TransItemName)
+				#input_box.send_keys(Keys.CONTROL, 'v')
 
-			# 
+				#driver.implicitly_wait(7)
+				time.sleep(10)
 
-			name_list = trans_box.text
-			"""
-			f = open("C:/xampp/htdocs/_Ntos/_TransSelenium/test_"+process+"_.txt", "w",encoding='UTF-8')
-			f.write(name_list)
-			f.close()
-			"""
-			data = {'CustId':CustId, 'Pc':Pc, 'Number': process, 'Mode':'transup', 'TransOrigin':TransOrigin, 'TransUp':TransUp, 'codelist':TransItemCode, 'namelist' : name_list, 'orgnamelist' : TransItemName } 
-			try :
-				response = requests.post(NtosUrl, data=data)
-			except :
-				time.sleep(2)
-				response = requests.post(NtosUrl, data=data)
-			"""
-			print("====================" + process + "=========================")
-			print(name_list)
-			"""
-			Result = response.text
-			Time_ = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-			print("Pc"+Pc+" ("+process+") > "+Result+" : "+ Time_)
+				if TransSite == "Papago" :
+					trans_box = driver.find_element_by_xpath('/html/body/div/div/div[1]/section/div/div[1]/div[2]/div/div[5]/div')
+				else :
+					trans_box = driver.find_element_by_xpath('/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div[5]/div/div[1]')
+
+				#driver.find_element_by_xpath('/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div[5]/div/div[4]/div[2]/div/span/button/i').click()
+
+				# 
+
+				name_list = trans_box.text
+				"""
+				f = open("C:/xampp/htdocs/_Ntos/_TransSelenium/test_"+process+"_.txt", "w",encoding='UTF-8')
+				f.write(name_list)
+				f.close()
+				"""
+				data = {'CustId':CustId, 'Pc':Pc, 'Number': process, 'Mode':'transup', 'TransOrigin':TransOrigin, 'TransUp':TransUp, 'codelist':TransItemCode, 'namelist' : name_list, 'orgnamelist' : TransItemName } 
+				try :
+					response = requests.post(NtosUrl, data=data)
+				except :
+					time.sleep(2)
+					response = requests.post(NtosUrl, data=data)
+				"""
+				print("====================" + process + "=========================")
+				print(name_list)
+				"""
+				Result = response.text
+				Time_ = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+				print("Pc"+Pc+"["+process+"] >>> "+Result+" : "+ Time_)
 
 
-			driver.close();
+				driver.close();
 
 
 if __name__ == '__main__':
