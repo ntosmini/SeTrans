a9c020ff29c90730814bf415d974587d8db61c98
jungmini <mins0428@gmail.com>
2021-08-11 09:31:45 +0900

    Ntos

---
+++ Trans.py
@@ -0,0 +1,196 @@
+import sys
+import os
+
+import requests
+
+import selenium
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+
+from time import sleep
+import threading
+
+import multiprocessing
+
+#import pyperclip
+
+
+
+# 실행 PC 리스트 가져오기
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from _common import process_list
+
+
+
+
+
+
+
+# 번역
+def multiSelenium(process):
+
+	slp = (int(process) - 1) * 9
+	sleep(slp)
+
+	# 실행 PC 리스트 가져오기
+	sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+	from _common import Pc
+	from _common import Server
+
+
+	from _Run import TransType
+	TransSite = TransType[0]	#Papago, Google
+	NtosUrl = TransType[1]	#Ntos 번역 Url
+	CustId = TransType[2]	#Ntos Id
+	TransLeng = TransType[3]	#번역사이트 언어설정
+	TransOrigin = TransType[4]	#번역할 필드
+	TransUp = TransType[5]	#업데이트할 필드
+	#TransSite = "push"
+	if TransSite == "" :
+		print("번역 미실행중")
+	elif TransSite == "push" :
+		os.system('cd C:\\xampp\\htdocs\\_Ntos\\_TransSelenium')
+		os.system('git status')
+		os.system('git add .')
+		os.system('git commit -m "NtosMini"')
+		os.system('git push -u origin master')
+	elif TransSite == "pull" :
+		pass
+	else : 
+
+		data = {'CustId':CustId, 'Pc':Pc, 'Number': process, 'Mode':'list', 'TransOrigin':TransOrigin, 'TransUp':TransUp } 
+		response = requests.post(NtosUrl, data=data)
+
+
+
+		#result = "".join(sampleText)
+		result = response.text
+		
+		if result == "test" :
+			if process == "1" :
+				print("실행명령")
+			else :
+				print("중지")
+		elif result == "error" :
+			print('에러')
+		elif result == "not" :
+			print('번역없음')
+		else :
+			
+			#번역시작
+			resultList = result.split("@@@")
+			number = 0
+			itemcodeArr = []
+			itemnameArr = []
+
+			for val in resultList :
+				(itemcode,itemname) = val.split("|@|")
+				itemcodeArr.insert(number, itemcode)
+				itemnameArr.insert(number, itemname.replace("\n", " "))
+				number = number +1
+
+			TransItemCode = "|@|".join(itemcodeArr)
+			TransItemName = "\n\n\n\n".join(itemnameArr)
+			"""
+			f = open("C:/xampp/htdocs/_Ntos/_TransSelenium/test_"+process+".txt", "w",encoding='UTF-8')
+			f.write(TransItemName)
+			f.close()
+			"""
+			"""
+			print('번역실행'+TransItemName)
+			"""
+			"""
+			options = webdriver.ChromeOptions()
+			options.add_argument('window-size=1920,1080')
+			options.add_argument('HEADLESS')
+			"""
+			chrome_options = webdriver.ChromeOptions()
+			chrome_options.add_argument('HEADLESS')
+			chrome_options.add_argument("window-size=1920,1080")
+
+			if TransSite == "Papago" :
+				URL = 'https://papago.naver.com/?'+ str(TransLeng)
+			else :
+				URL = 'https://translate.google.com/?'+ str(TransLeng)
+			if Server == "win" :
+				driver = webdriver.Chrome('C:/xampp/htdocs/_Ntos/_TransSelenium/chromedriver.exe', chrome_options=chrome_options)
+				#driver = webdriver.Chrome('C:/xampp/htdocs/_Ntos/chromedriver.exe', options=options)
+				#driver = webdriver.Chrome('C:/xampp/htdocs/_Ntos/chromedriver.exe')
+			else :	#ubuntu
+				driver = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
+			driver.implicitly_wait(3)
+
+			driver.get(url=URL)
+			driver.implicitly_wait(3)
+
+			#종료
+			"""
+			def DriverQuit():
+				terminate(process)
+				driver.close()
+
+			DriverJob = threading.Timer(50, DriverQuit)
+			DriverJob.start()
+			"""
+
+			if TransSite == "Papago" :
+				input_box = driver.find_element_by_xpath('/html/body/div/div/div[1]/section/div/div[1]/div[1]/div/div[3]/label/textarea')
+			else :
+				input_box = driver.find_element_by_xpath('/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[1]/span/span/div/textarea')
+			sleep(1)
+			
+			input_box.send_keys(TransItemName)
+			#pyperclip.copy(TransItemName)
+			#input_box.send_keys(Keys.CONTROL, 'v')
+
+			#driver.implicitly_wait(7)
+			sleep(10)
+
+			if TransSite == "Papago" :
+				trans_box = driver.find_element_by_xpath('/html/body/div/div/div[1]/section/div/div[1]/div[2]/div/div[5]/div')
+			else :
+				trans_box = driver.find_element_by_xpath('/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div[5]/div/div[1]')
+
+			#driver.find_element_by_xpath('/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div[5]/div/div[4]/div[2]/div/span/button/i').click()
+
+			# 
+
+			name_list = trans_box.text
+			"""
+			f = open("C:/xampp/htdocs/_Ntos/_TransSelenium/test_"+process+"_.txt", "w",encoding='UTF-8')
+			f.write(name_list)
+			f.close()
+			"""
+			data = {'CustId':CustId, 'Pc':Pc, 'Number': process, 'Mode':'transup', 'TransOrigin':TransOrigin, 'TransUp':TransUp, 'codelist':TransItemCode, 'namelist' : name_list, 'orgnamelist' : TransItemName } 
+			try :
+				response = requests.post(NtosUrl, data=data)
+			except :
+				sleep(2)
+				response = requests.post(NtosUrl, data=data)
+			"""
+			print("====================" + process + "=========================")
+			print(name_list)
+			"""
+			print(response.text)
+
+
+			driver.close();
+
+
+if __name__ == '__main__':
+
+	# 프로세스 3개
+	#process_list = ["1", "2", "3"] #../_common.py
+
+	# 멀티 프로세스 사용
+	pool = multiprocessing.Pool(processes=len(process_list))
+	pool.map(multiSelenium, process_list)
+	pool.close()
+	pool.join()
---
+++ _Run.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*- 
+
+#기본설정
+
+import requests
+
+NtosTransConfigUrl = "http://mini.ntos.co.kr/_Mini_/_TransSelenium/_Trans.txt";
+response = requests.get(NtosTransConfigUrl)
+
+TransMode = response.text
+
+
+
+
+"""
+amazon.com en => ko
+
+amazon.de de => en
+amazon.de en => ko
+
+
+
+"""
+
+if TransMode == "mini_de_en" :
+	TransType = [
+		'Google'	#Papago, Google
+		, 'http://mini.ntos.co.kr/_Mini_/_TransSelenium/trans.php'	#Ntos 번역 Url
+		, 'mini'		#CustId (Ntos Id)
+		, 'sl=de&tl=en'	#번역사이트 언어설정
+		, 'it_name'	#번역할 필드
+		, 'it_name_en'	#업데이트할 필드
+		]
+elif TransMode == "mini_en_ko" :
+	TransType = [
+		'Papago'	#Papago, Google
+		, 'http://mini.ntos.co.kr/_Mini_/_TransSelenium/trans.php'	#Ntos 번역 Url
+		, 'mini'		#CustId (Ntos Id)
+		, 'sk=en&tk=ko'	#번역사이트 언어설정
+		, 'it_name_en'	#번역할 필드
+		, 'it_name_ko'	#업데이트할 필드
+		]
+
+
+elif TransMode == "com_en_ko" :
+	TransType = [
+		'Papago'	#Papago, Google
+		, 'http://amazon.ntos.co.kr/_Mini_/_TransSelenium/trans.php'	#Ntos 번역 Url
+		, 'amazon'		#CustId (Ntos Id)
+		, 'sk=en&tk=ko'	#번역사이트 언어설정
+		, 'it_name'	#번역할 필드
+		, 'it_name_ko'	#업데이트할 필드
+		]
+elif TransMode == "de_de_en" :
+	TransType = [
+		'Google'	#Papago, Google
+		, 'http://amazonde.ntos.co.kr/_Mini_/_TransSelenium/trans.php'	#Ntos 번역 Url
+		, 'amazon'		#CustId (Ntos Id)
+		, 'sl=de&tl=en'	#번역사이트 언어설정
+		, 'it_name'	#번역할 필드
+		, 'it_name_en'	#업데이트할 필드
+		]
+elif TransMode == "de_en_ko" :
+	TransType = [
+		'Papago'	#Papago, Google
+		, 'http://amazonde.ntos.co.kr/_Mini_/_TransSelenium/trans.php'	#Ntos 번역 Url
+		, 'amazon'		#CustId (Ntos Id)
+		, 'sk=en&tk=ko'	#번역사이트 언어설정
+		, 'it_name_en'	#번역할 필드
+		, 'it_name_ko'	#업데이트할 필드
+		]
+elif TransMode == "push" :	#git push
+	TransType = [
+		'push'	#Papago, Google
+		, ''	#Ntos 번역 Url
+		, ''		#CustId (Ntos Id)
+		, ''	#번역사이트 언어설정
+		, ''	#번역할 필드
+		, ''	#업데이트할 필드
+		]
+
+elif TransMode == "pull" :	#git pull
+	TransType = [
+		'pull'	#Papago, Google
+		, ''	#Ntos 번역 Url
+		, ''		#CustId (Ntos Id)
+		, ''	#번역사이트 언어설정
+		, ''	#번역할 필드
+		, ''	#업데이트할 필드
+		]
+
+
+else :
+	TransType = [
+		''	#Papago, Google
+		, ''	#Ntos 번역 Url
+		, ''		#CustId (Ntos Id)
+		, ''	#번역사이트 언어설정
+		, ''	#번역할 필드
+		, ''	#업데이트할 필드
+		]
