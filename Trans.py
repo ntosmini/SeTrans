e6fa61a369976848749f857ef3f07a2c8db88749
jungmini <mins0428@gmail.com>
2021-09-27 09:21:32 +0900

    Update Trans.py

--- Trans.py
+++ Trans.py
@@ -47,6 +47,29 @@ if time.strftime('%M', time.localtime(time.time())) == "00" :
 				parent.kill()
 		except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):   #예외처리
 			pass
+	time.sleep(1)
+
+
+if time.strftime('%M', time.localtime(time.time())) == "100" :
+	# 프로세스 죽이기
+	import psutil   # 실행중인 프로세스 및 시스템 활용 라이브러리
+	print('Kill Start... '+time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+	for proc in psutil.process_iter():
+		try:
+			# 프로세스 이름, PID값 가져오기
+			processName = proc.name()
+			processID = proc.pid
+			#print(processName , ' - ', processID)
+	 
+			if processName == "chrome.exe" or processName == "chromedriver.exe" :
+				print('kill - ', processName , ' - ', processID)
+				parent_pid = processID  #PID
+				parent = psutil.Process(parent_pid) # PID 찾기
+				for child in parent.children(recursive=True):  #자식-부모 종료
+					child.kill()
+				parent.kill()
+		except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):   #예외처리
+			pass
 	time.sleep(5)
 else :
 	
